b52501bd5f48b8ab370a0d41c022695e6ca87710
Gribnoff <quinvex88@gmail.com>
2020-02-29 23:31:56 +0300

    books list and add via thymeleaf

---
+++ src/main/java/l5/Book.java
@@ -0,0 +1,56 @@
+package l5;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "year")
+    private int year;
+
+    @Column(name = "short_description")
+    private String shortDesc;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public void setShortDesc(String shortDesc) {
+        this.shortDesc = shortDesc;
+    }
+
+    public Book() {
+    }
+}
---
+++ src/main/java/l5/BookController.java
@@ -0,0 +1,41 @@
+package l5;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/books")
+class BookController {
+    private BookService bookService;
+
+    @SuppressWarnings("unused")
+    @Autowired
+    void setBookService(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @SuppressWarnings("unused")
+    @GetMapping("/catalog")
+    public String showCatalog(Model model) {
+        model.addAttribute("books", bookService.getBookList());
+        return "books-list";
+    }
+
+    @SuppressWarnings("unused")
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "add-book-form";
+    }
+
+    @PostMapping("/add")
+    public String addBook(Book book) {
+        bookService.addBook(book);
+        return "redirect:/books/catalog";
+    }
+}
