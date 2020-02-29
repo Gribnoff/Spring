a17fa055ebcbcd71ff8cdcf889a04ad54a185210
Gribnoff <quinvex88@gmail.com>
2020-02-29 23:31:56 +0300

    books list and add via thymeleaf

---
+++ src/main/java/l5/BookService.java
@@ -0,0 +1,25 @@
+package l5;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    private BookRepository bookRepository;
+
+    @SuppressWarnings("unused")
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    List<Book> getBookList() {
+        return (List<Book>) bookRepository.findAll();
+    }
+
+    void addBook(Book book) {
+        bookRepository.save(book);
+    }
+}
