1851fe194f35a23d885fdaa11c117d8fb266462c
Gribnoff <quinvex88@gmail.com>
2020-03-03 17:41:22 +0300

    security configuration

---
+++ src/main/java/l6/SecurityConfig.java
@@ -0,0 +1,22 @@
+package l6;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder users = User.builder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("user1").password("{noop}pass1").roles("USER", "ADMIN"))
+                .withUser(users.username("user2").password("{noop}pass2").roles("USER"));
+    }
+}
---
+++ src/main/java/l6/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package l6;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.stereotype.Component;
+
+@Component
+class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
