de9fc827b99298b51212ac8905b899bcb134899a
Gribnoff <quinvex88@gmail.com>
2020-03-03 17:37:05 +0300

    controllers for login & index pages

---
+++ src/main/java/l6/LoginController.java
@@ -0,0 +1,13 @@
+package l6;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @SuppressWarnings("unused")
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login-page";
+    }
+}
