ba24998c1a48ea8d25b0f0e65735c2247dcbde9e
Gribnoff <quinvex88@gmail.com>
2020-03-03 17:57:10 +0300

    method security

--- src/main/java/l6/MainController.java
+++ src/main/java/l6/MainController.java
@@ -1,5 +1,6 @@
 package l6;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,6 +9,13 @@ public class MainController {
     @SuppressWarnings("unused")
     @GetMapping("/")
     public String goHome() {
+        return "index";
+    }
+
+    @SuppressWarnings("unused")
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/onlyAdmin")
+    public String onlyAdmin() {
         return "index";
     }
 }
