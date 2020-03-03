d328a05a427c21d2f04c04f0516678d2f7ac570d
Gribnoff <quinvex88@gmail.com>
2020-03-03 17:42:25 +0300

    url security

--- src/main/java/l6/SecurityConfig.java
+++ src/main/java/l6/SecurityConfig.java
@@ -3,6 +3,7 @@ package l6;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -18,5 +19,16 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser(users.username("user1").password("{noop}pass1").roles("USER", "ADMIN"))
                 .withUser(users.username("user2").password("{noop}pass2").roles("USER"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("USER", "ADMIN")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll();
     }
 }
